3a1c5dbbcd9fa5e23462b3b54346a0efff5877ac
tannguyentokyo <tannguyen.vtl@gmail.com>
2024-09-21 12:45:48 +0900

    test

---
+++ spring_demo_1/src/main/java/com/example/spring_demo/test.java
@@ -0,0 +1,11 @@
+package com.example.spring_demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class test {
+	@RequestMapping("/a")
+	public int tinhTong() {
+		return 1+2+5;
+	}
+}
