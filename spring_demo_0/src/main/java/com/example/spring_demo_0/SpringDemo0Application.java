ce40d9270b801d42ae02581896edbf02f10b1ad6
tannguyentokyo <tannguyen.vtl@gmail.com>
2024-09-21 16:08:24 +0900

    test demo

---
+++ spring_demo_0/src/main/java/com/example/spring_demo_0/SpringDemo0Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_demo_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemo0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemo0Application.class, args);
+	}
+
+}
